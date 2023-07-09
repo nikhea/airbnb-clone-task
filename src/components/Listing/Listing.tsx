cefaa051fae1b9231e78ef4ad8138ba43821afa9
nikhea <imonikheaugbodaga@gmail.com>
2023-07-09 16:12:05 +0100

    fix zustand store

--- src/components/Listing/Listing.tsx
+++ src/components/Listing/Listing.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import ListingCard from "./ListingCard";
 import Container from "../UI/Container";
 import { useListingStore } from "@/hooks/useListingStore";
+import { ListingItem } from "@/types/ListingItem";
+import { ListingData } from "@/data/listingData";
 
 const Listing = () => {
-  const { listingData } = useListingStore();
-  const displayListing = listingData.map((listing, index) => (
-    <ListingCard data={listing} key={index} />
-  ));
+  // const { listingData } = useListingStore();
+  const displayListing = ListingData.map(
+    (listing: ListingItem, index: number) => (
+      <ListingCard data={listing} key={index} />
+    )
+  );
   return (
     <Container>
       <div className="grid grid-cols-1 gap-3 my-3 md:grid-cols-2 lg:grid-cols-4">
