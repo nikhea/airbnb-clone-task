5dc3510a76d20e156803a41f9c6375e32055cd03
nikhea <imonikheaugbodaga@gmail.com>
2023-07-10 18:52:02 +0100

    wip: added map button style margin

--- src/components/Map/MapIcon.tsx
+++ src/components/Map/MapIcon.tsx
@@ -6,7 +6,7 @@ const MapIcon = () => {
     <div className="fixed cursor-pointer z-50 left-[45%] p-3 bottom-24 flex items-center justify-center ">
       <div className="flex items-center p-3 text-white bg-[#222222] rounded-3xl">
         <h1>
-          <span className="hidden md:block">Show</span> map{" "}
+          <span className="items-center hidden md:flex">Show</span> map{" "}
         </h1>
         <FaMap className="ml-2" />
       </div>
