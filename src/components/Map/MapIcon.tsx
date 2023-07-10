39b1773d2f7e233ffd6a48db27a66d190c8dc047
nikhea <imonikheaugbodaga@gmail.com>
2023-07-10 19:12:42 +0100

    fix: adjusted map button for mobile

--- src/components/Map/MapIcon.tsx
+++ src/components/Map/MapIcon.tsx
@@ -3,7 +3,7 @@ import { FaMap } from "react-icons/fa";
 
 const MapIcon = () => {
   return (
-    <div className="fixed cursor-pointer z-50 left-[45%] p-3 bottom-24 flex items-center justify-center ">
+    <div className="fixed cursor-pointer z-50 left-[45%] p-3 bottom-32 lg:bottom-24 flex items-center justify-center ">
       <div className="flex items-center p-3 px-5 text-white bg-[#222222] rounded-3xl">
         <h1 className="flex items-center ">
           <span className="hidden ml-1 md:flex">Show</span> map{" "}
