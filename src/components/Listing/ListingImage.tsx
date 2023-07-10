333a44848a3233c157714be650330e802749d262
nikhea <imonikheaugbodaga@gmail.com>
2023-07-10 23:54:22 +0100

    adedd swiper

--- src/components/Listing/ListingImage.tsx
+++ src/components/Listing/ListingImage.tsx
@@ -1,28 +1,57 @@
+"use client";
+import "swiper/swiper-bundle.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  EffectCoverflow,
+} from "swiper";
 import React, { FC } from "react";
 import Image, { StaticImageData } from "next/image";
 import HeartButton from "./HeartButton";
 import { ListingItem } from "@/types/ListingItem";
-
+import SliderButton from "../UI/SliderButton";
+SwiperCore.use([
+  Navigation,
+  Pagination,
+  EffectCoverflow,
+  EffectCoverflow,
+  Autoplay,
+]);
 type ListingImage = {
   images: StaticImageData[] | string[];
   data: ListingItem;
 };
 export const ListingImage: FC<ListingImage> = ({ images, data }) => {
   const displayImages = images.map((image, index) => (
-    <div
-      key={index}
-      className="relative w-full overflow-hidden h-[300px]  rounded-xl"
-    >
-      <Image
-        fill
-        className="object-cover w-full h-full transition group-hover:scale-110"
-        src={image}
-        alt="Listing"
-      />
-      <div className="absolute top-3 right-3">
-        <HeartButton itemID={data.id} isFavorite={data.isFavorite} />
+    <SwiperSlide key={index}>
+      <div className="relative w-full overflow-hidden h-[300px]  rounded-xl">
+        <Image
+          fill
+          className="object-cover w-full h-full transition group-hover:scale-110"
+          src={image}
+          alt="Listing"
+        />
+        <div className="absolute top-3 right-3">
+          <HeartButton itemID={data.id} isFavorite={data.isFavorite} />
+        </div>
       </div>
-    </div>
+    </SwiperSlide>
   ));
-  return <div>{displayImages}</div>;
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      slidesPerView={1}
+    >
+      <div> {displayImages}</div>
+      <SliderButton />
+    </Swiper>
+  );
 };
