53cc9328307d66ba1a6ccb5ffe4135844c667c8a
nikhea <imonikheaugbodaga@gmail.com>
2023-07-10 09:11:35 +0100

    wip: adding Footer style

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -10,17 +10,29 @@ const Footer = () => {
           <div className={style.footer__Items}>
             <div className="flex flex-row items-center gap-x-2">
               <h2 className="capitalize">@2023 Airbnb, inc</h2>
-              <ul className="flex flex-row items-center capitalize gap-x-2">
-                <li>. terms</li>
-                <li>. sitmap</li>
-                <li>. privacy</li>
-                <li>. your privacy choices</li>
-                <li>. destinations</li>
+              <ul className="flex flex-row items-center capitalize gap-x-2 font-normal text-sm text-[#222]">
+                <li className="flex flex-row items-center">
+                  <span>.</span> terms
+                </li>
+                <li className="flex flex-row items-center">
+                  <span>.</span> sitmap
+                </li>
+                <li className="flex flex-row items-center">
+                  <span>.</span> privacy
+                </li>
+                <li className="flex flex-row items-center">
+                  <span>.</span> your privacy choices
+                </li>
+                <li>
+                  <span>.</span> destinations
+                </li>
               </ul>
             </div>
             <div>
-            <ul className="flex flex-row items-center capitalize gap-x-2 text-bold">
-                <li className="flex flex-row items-center">  <TbWorld size={20} /> english (US)</li>
+              <ul className="flex flex-row items-center capitalize gap-x-2 font-extrabold ">
+                <li className="flex flex-row items-center">
+                  <TbWorld size={20} className="mr-2" /> english (US)
+                </li>
                 <li>$ USD</li>
                 <li>. privacy</li>
                 <li>support & resources</li>
