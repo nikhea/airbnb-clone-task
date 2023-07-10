a8008781ef67dee08717ffeb874d029939572d5c
nikhea <imonikheaugbodaga@gmail.com>
2023-07-10 16:23:07 +0100

    fix: responsive nav icon

--- src/components/layout/Navbar/Search.tsx
+++ src/components/layout/Navbar/Search.tsx
@@ -17,18 +17,20 @@ const Search = () => {
         </div>
       </div>
       <div className={style.search__SmContainer}>
-        <div>
+        <div className="flex items-center gap-x-3">
           <BiSearch size={18} />
-          <div>
-            <div className={style.search__text}>Anywhere</div>
-            <div>
-              <div className={style.search__textBorder}>Any week</div>
-              <div className={style.guest__text}>Add guests </div>
+          <div className="flex flex-col items-center ">
+            <div>Anywhere</div>
+            <div className="flex items-center font-thin text-[##717171]">
+              <div>Any week</div>
+              <div>
+                <span className="mr-1">.</span>Add guests{" "}
+              </div>
             </div>
           </div>
         </div>
-        <div className={style.guest}>
-          <div className={style.guest__icon}>
+        <div>
+          <div className="border-[1px] w-full  md:w-auto py-2 rounded-full  shadow-sm">
             <FilterIcon />
           </div>
         </div>
