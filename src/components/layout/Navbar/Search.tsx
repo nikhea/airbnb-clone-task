7b065e036c5a5c2fcbe883d430bf77b6e62f6705
nikhea <imonikheaugbodaga@gmail.com>
2023-07-10 16:10:55 +0100

    fix: responsive nav

--- src/components/layout/Navbar/Search.tsx
+++ src/components/layout/Navbar/Search.tsx
@@ -1,17 +1,35 @@
 "use client";
 import { BiSearch } from "react-icons/bi";
 import style from "./style/search.module.scss";
+import FilterIcon from "@/components/UI/ICons/FIliterIcon";
 
 const Search = () => {
   return (
     <div className={style.search}>
-      <div className={style.search__Container}>
+      <div className={style.search__LgContainer}>
         <div className={style.search__text}>Anywhere</div>
         <div className={style.search__textBorder}>Any week</div>
         <div className={style.guest}>
           <div className={style.guest__text}>Add guests </div>
           <div className={style.guest__icon}>
             <BiSearch size={18} />
+          </div>
+        </div>
+      </div>
+      <div className={style.search__SmContainer}>
+        <div>
+          <BiSearch size={18} />
+          <div>
+            <div className={style.search__text}>Anywhere</div>
+            <div>
+              <div className={style.search__textBorder}>Any week</div>
+              <div className={style.guest__text}>Add guests </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.guest}>
+          <div className={style.guest__icon}>
+            <FilterIcon />
           </div>
         </div>
       </div>
